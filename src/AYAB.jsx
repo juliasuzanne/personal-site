127601f9958dfdfac1e0fd8ed4d8a5f2d50d3966
Julia Grimes <julia.s.grimes@gmail.com>
2023-02-16 12:36:33 -0500

    Formatting ayab page

--- src/AYAB.jsx
+++ src/AYAB.jsx
@@ -6,16 +6,16 @@ export function AYAB() {
         home{" "}
       </a>
       <br></br>
-      <h1>
+      <h1 className="header">
         {" "}
         Experimenting with open source software, <a href="https://ayab-knitting.com/"> AYAB </a>{" "}
       </h1>
-      <p>
+      <p className="fonted">
         {" "}
         During the pandemic, I discovered a project called All Yarns Are Beautiful. It's open source software that
         connects a laptop to an 80s knitting machine to create custom textiles.
       </p>
-      <p>
+      <p className="fonted">
         I bought a cheap knitting machine off eBay, restored it, soldered the custom Arduino shield following tutorials,
         and installed the software. I am documenting my work using it here!
       </p>
