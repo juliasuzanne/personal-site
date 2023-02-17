cc5af8de088f262797747327d56c81c1ca095430
Julia Grimes <julia.s.grimes@gmail.com>
2023-02-17 11:52:03 -0500

    Added unity video, styling, and added content to AYAB

--- src/AYAB.jsx
+++ src/AYAB.jsx
@@ -20,6 +20,51 @@ export function AYAB() {
         and installed the software. I am documenting my work using it here!
       </p>
       <img className="unity_container" src="/images/ayab.jpg" />
+      <div className="calibration">
+        <h3 className="header">Calibration and Troubleshooting</h3>
+        <p className="fonted">
+          {" "}
+          Once I restored and hacked the machine initially, I did some small test runs of icons like flowers and text.
+        </p>
+        <p className="fonted">
+          Then, I had to figure out what kinds of images work best - strong shadows and large, easily recognizable
+          features, no big gaps of negative space. I created a custom template to evenly space out B and W within a few
+          stitches of each other to avoid large stretches of yarn looping around the other side.{" "}
+        </p>
+        <div className="statue">
+          <img src="/images/venusdemilo.jpg" />
+          <img width="130" src="/images/statue.png" />
+        </div>
+        <p className="fonted">
+          {" "}
+          You can see how the image had to be altered to include black and white dotted pattern to avoid large areas of
+          one color. You can also notice how much the image gets stretched out, I had to calibrate that by streching the
+          image by a factor of 3.{" "}
+        </p>
+        <p className="fonted">
+          The last major challenge at this step was removing the glitchy vertical lines. This was a mechanical error,
+          caused by bent needles. I replaced them and the foam bar they rest on.
+        </p>
+      </div>
+      <h3 className="header"> Continuous Experimentation</h3>
+      <p className="fonted">
+        I am continuing to experiment with what thread gauges work best, some additional glitching that's happening, and
+        what materials are easiest to work with and sew afterwards. I've been serging the edges, but I'd like to find a
+        way to quickly and more elegantly finish the edges.
+      </p>
+      <img src="/images/dress-front.png" />
+      <img src="/images/dress.png" />
+      <p className="fonted">
+        {" "}
+        I really enjoy the trompe l'oeil work of Jean-Paul Gaultier and others in the fashion industry, so I have been
+        experimenting with using this "printing" method to create my own trompe l'oeil garments.{" "}
+      </p>
+      <img width="300px" src="/images/audrey.png" />
+      <img width="400px" src="/images/jacket.png" />
+      <p className="fonted">
+        I also tried felting with wool thread, with mixed results. It shrinks so much it becomes very expensive. It's
+        possible to get a higher resolution per inch, though. Pictures to follow.
+      </p>
     </div>
   );
 }
--- src/Unity.jsx
+++ src/Unity.jsx
@@ -18,6 +18,14 @@ export function Unity() {
         </p>
       </div>
       <img className="unity_container" src="/images/unity_game.png" />
+
+      <div id="unityvideo">
+        <iframe className="unityvideo" controls="1" src="/images/unity-demo.mp4" title="video player"></iframe>
+
+        <a href="https://github.com/juliasuzanne/2D_unity_scripting_101" className="unity_description_2">
+          Github Repo
+        </a>
+      </div>
     </div>
   );
 }
