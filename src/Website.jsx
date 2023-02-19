07c8a2612c8ec592ecc921c59daf0be02f7e74dc
Julia Grimes <julia.s.grimes@gmail.com>
2023-02-19 11:25:58 -0500

    Added modal with additional information to detail shows. Changed link to open in new tab rather than change current tab

--- src/Website.jsx
+++ src/Website.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import { Modal } from "./Modal";
+
 export function Website() {
+  const [currentImage, setCurrentImage] = useState("");
+
+  const [textDescription, setTextDescription] = useState();
+  const [showImage, setShowImage] = useState(false);
+
+  const handleShowImage = () => {
+    setShowImage(true);
+  };
+
+  const handleHideImage = () => {
+    setShowImage(false);
+  };
+
   return (
     <div>
       <a className="link" id="home" href="/">
@@ -7,7 +23,7 @@ export function Website() {
       </a>
       <br></br>
       <h2 className="header"> Experiments with React - Point and Click style interaction with original artwork</h2>
-      <a href="https://www.moon--egg.com/">
+      <a href="https://www.moon--egg.com/" target="_blank">
         <h3 className="header"> Visit the live site here! </h3>
       </a>
       <p className="fonted">
@@ -30,13 +46,81 @@ export function Website() {
           title="video player"
         ></iframe>
       </div>
+      <Modal show={showImage} onClose={handleHideImage}>
+        <img id="modalImage" src={currentImage} />
+        <p className="fonted"> {textDescription} </p>
+      </Modal>
       <div className="games_images">
-        <img className="game_image" id="two" src="/images/8.png" />
-        <img className="game_image" id="two" src="/images/9.png" />
-        <img className="game_image" id="one" src="/images/1.png" />
-        <img className="game_image" id="two" src="/images/2.png" />
-        <img className="game_image" id="two" src="/images/4.png" />
-        <img className="game_image" id="two" src="/images/6.png" />
+        <img
+          className="game_image"
+          id="two"
+          src="/images/8.png"
+          onClick={() => {
+            setCurrentImage("/images/8.png");
+            handleShowImage();
+            setTextDescription(
+              "This is an example of one of the backend interfaces I built so that I could edit the database live via the web browser."
+            );
+          }}
+        />
+        <img
+          className="game_image"
+          id="two"
+          src="/images/9.png"
+          onClick={() => {
+            setCurrentImage("/images/9.png");
+            handleShowImage();
+            setTextDescription(
+              "This is a mini-game, snake. An array updates live to detect collisions with the 'apple', change direction and length of the snake, and points earned are connected to the database, saving points to the current user."
+            );
+          }}
+        />
+        <img
+          className="game_image"
+          id="one"
+          src="/images/1.png"
+          onClick={() => {
+            setCurrentImage("/images/1.png");
+            handleShowImage();
+            setTextDescription("Showing here the modal that reveals a dialog box to interact with the cashier.");
+          }}
+        />
+        <img
+          className="game_image"
+          id="two"
+          src="/images/2.png"
+          onClick={() => {
+            setCurrentImage("/images/2.png");
+            handleShowImage();
+            setTextDescription(
+              "Showing a detail of the puzzle to fix the electrical wires and then turn on the lights. I used buttons and conditionals to achieve this. Completing this task earns the user 500 points, and the game will remember that this was done via a boolean variable assigned to each user."
+            );
+          }}
+        />
+        <img
+          className="game_image"
+          id="two"
+          src="/images/4.png"
+          onClick={() => {
+            setCurrentImage("/images/4.png");
+            handleShowImage();
+            setTextDescription(
+              "I had a lot of fun experimenting with adding interactive 3D models created in Blender to the site using Three.js. I'm looking for ways to do more of this."
+            );
+          }}
+        />
+        <img
+          className="game_image"
+          id="two"
+          src="/images/6.png"
+          onClick={() => {
+            setCurrentImage("/images/6.png");
+            handleShowImage();
+            setTextDescription(
+              "Detail showing the cataloging system for the back room inventory where users can convert items into artifacts for sale in the main room, to be purchased and then added to a user's inventory (if they have enough points)!"
+            );
+          }}
+        />
       </div>
     </div>
   );
