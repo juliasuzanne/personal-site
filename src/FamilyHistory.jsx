60aab0995410a2d57c4f2c9736cdd5dbb6f4138e
Julia Grimes <julia.s.grimes@gmail.com>
2023-03-06 12:16:18 -0500

    Added link to family history site

--- src/FamilyHistory.jsx
+++ src/FamilyHistory.jsx
@@ -4,10 +4,17 @@ export function FamilyHistory() {
       <h2 className="header">Yee Family History Project </h2>
       <p className="fonted"> My mom's side of the family, working on recording history.</p>
       <p className="fonted">pictures, interactive family tree, and recipes coming soon!</p>
-      <a className="header" href="https://github.com/juliasuzanne/family_tree_gojs">
+      <a className="header" href="https://github.com/juliasuzanne/family-tree-frontend">
         {" "}
         GitHub Repo here
       </a>
+      <a className="header" href="https://yeefamilyhistory.org/org_chart.html">
+        Website with Interactive Family Tree
+      </a>
+      <p>
+        Website is still being developed, but so far I have used GoJS to create an interactive family tree with photos,
+        birthday, and custom colors and styling.
+      </p>
     </div>
   );
 }
