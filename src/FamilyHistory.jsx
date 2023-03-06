f1af6077e854339e12b03b70df64028577c929b4
Julia Grimes <julia.s.grimes@gmail.com>
2023-03-06 12:19:22 -0500

    Changed links on family history page to open in new tabs

--- src/FamilyHistory.jsx
+++ src/FamilyHistory.jsx
@@ -4,11 +4,11 @@ export function FamilyHistory() {
       <h2 className="header">Yee Family History Project </h2>
       <p className="fonted"> My mom's side of the family, working on recording history.</p>
       <p className="fonted">pictures, interactive family tree, and recipes coming soon!</p>
-      <a className="header" href="https://github.com/juliasuzanne/family-tree-frontend">
+      <a className="header" href="https://github.com/juliasuzanne/family-tree-frontend" target="_blank">
         {" "}
         GitHub Repo here
       </a>
-      <a className="header" href="https://yeefamilyhistory.org/org_chart.html">
+      <a className="header" href="https://yeefamilyhistory.org/org_chart.html" target="_blank">
         Website with Interactive Family Tree
       </a>
       <p className="fonted">
